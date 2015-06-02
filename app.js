1dcd20b4e01cb010bc69bff75439b0eec70d0e14
abyrn <abyrn@users.noreply.github.com>
2015-06-01 19:30:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-something
+@@ -0,0 +1,53 @@
+angular.module('app', ["firebase"]);
+
+angular.module('app').controller('InitCtrl', function ($scope, $firebaseArray, $firebaseObject) {
+
+		
+	var ref = new Firebase("https://encountermanager.firebaseio.com/Players");
+
+	$scope.Characters = $firebaseArray(ref);
+
+	var mons = new Firebase("https://encountermanager.firebaseio.com/Monsters");
+
+	$scope.Current = {};
+
+	$scope.Monsters = $firebaseObject(mons);		
+
+	$scope.addCharacters = function(character) {
+		var copyCharacter = {};
+		angular.copy(character, copyCharacter);
+		var hp = 0;
+		var hd = copyCharacter.hd.size.split(/[d, ]/)
+		for(i = 0; i < hd[0]; i++){
+			hp = hp + Math.floor(Math.random() * hd[1]) + 1
+		};
+		hp = eval(hp + hd[2] + hd[3]);
+		copyCharacter.hp = {};
+		copyCharacter.hp.max = hp; 
+		copyCharacter.hp.current = hp;
+		copyCharacter.acSel = character.ac[0];
+		$scope.Characters.$add(copyCharacter);
+	};
+
+	$scope.CurrentCharacter = function(character) {
+		var cur = $firebaseObject(ref.child(character.$id));
+		$scope.Current = cur;		
+	};
+
+	var spell = new Firebase("https://encountermanager.firebaseio.com/Spells");
+
+	$scope.Spells = $firebaseObject(spell);
+
+});
+
+angular.module('app').filter('scores', function() {
+	return function(input) {
+		var out = 0;
+		if (input >= 10) {
+			out = Math.floor((input - 10)/2)
+		} else {
+			out = Math.floor((input - 10)/2)
+		}
+		return out;	
+	};
+});
